38734c7a952446c844e34ef5d00ad1ef9b3102aa
Bjarke Buur Mortensen <mortensen@eluence.com>
2020-05-26 14:54:00 +0200

    Made action understand ruby ci_reporter output

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const glob = require('@actions/glob');
+const ospath = require('path');
 const parser = require('xml2json');
 const fs = require('fs');
 
-
-
-
-
+const MINITEST_TEST_FILE_PATH_RE = new RegExp('\\(Minitest::Assertion\\)[^\/]*((\/[^:]+):(\\d+))', 'm');
 
 (async () => {
     try {
-        const path = core.getInput('path');
+        const testReportsGlob = core.getInput('path');
         const includeSummary = core.getInput('includeSummary');
         const numFailures = core.getInput('numFailures');
         const accessToken = core.getInput('access-token');
         const testSrcPath = core.getInput('testSrcPath');
-        const globber = await glob.create(path, {followSymbolicLinks: false});
+        const annotationJobName = core.getInput('annotationJobName');
+        const globber = await glob.create(testReportsGlob, {followSymbolicLinks: false});
 
         let numTests = 0;
         let numSkipped = 0;
@@ -25,12 +24,11 @@ const fs = require('fs');
         let testDuration = 0;
 
         let annotations = [];
-       
 
         for await (const file of globber.globGenerator()) {
             const data = await fs.promises.readFile(file);
             var json = JSON.parse(parser.toJson(data));
-            if(json.testsuite) {
+            if (json.testsuite) {
                 const testsuite = json.testsuite;
                 testDuration +=  Number(testsuite.time);
                 numTests +=  Number(testsuite.tests);
@@ -38,33 +36,23 @@ const fs = require('fs');
                 numFailed +=  Number(testsuite.failures);
                 numSkipped +=  Number(testsuite.skipped);
                 testFunction = async testcase => {
-                    if(testcase.failure) {
-                        if(annotations.length < numFailures) {
-                            const klass = testcase.classname.replace(/$.*/g, '').replace(/\./g, '/');
-                            const path = `${testSrcPath}${klass}.java`
+                    if (annotations.length >= numFailures || !testcase.failure) {
+                        return;
+                    }
 
-                            const file = await fs.promises.readFile(path, {encoding: 'utf-8'});
-                            //TODO: make this better won't deal with methods with arguments etc
-                            let line = 0;
-                            const lines = file.split('\n')
-                                for(let i = 0; i < lines.length; i++) {
-                                if(lines[i].indexOf(testcase.name) >= 0) {
-                                    line = i;
-                                    break;
-                                }
-                            }
-                            annotations.push({
-                                path: path,
-                                start_line: line,
-                                end_line: line,
-                                start_column: 0,
-                                end_column: 0,
-                                annotation_level: 'failure',
-                                message: `Junit test ${testcase.name} failed ${testcase.failure.message}`,
-                              });
-                        }
-                        //add
-                    }      
+                    let fileMatch = testcase.failure.$t.match(MINITEST_TEST_FILE_PATH_RE);
+                    let path = fileMatch ? ospath.relative(process.cwd(), fileMatch[2]) : testSrcPath;
+                    let lineNumber = fileMatch ? fileMatch[3] : 0;
+
+                    annotations.push({
+                        path: path,
+                        start_line: lineNumber,
+                        end_line: lineNumber,
+                        start_column: 0,
+                        end_column: 0,
+                        annotation_level: 'failure',
+                        message: `Junit test ${testcase.name} failed:\n ${testcase.failure.message}`,
+                    });
                 }
 
                 if(Array.isArray(testsuite.testcase)) {
@@ -81,32 +69,22 @@ const fs = require('fs');
         const octokit = new github.GitHub(accessToken);
         const req = {
         ...github.context.repo,
-        ref: github.context.sha
+        ref: github.context.sha || core.getInput('commit-sha')
         }
         const res = await octokit.checks.listForRef(req);
     
-        const check_run_id = res.data.check_runs.filter(check => check.name === 'build')[0].id
+        const check_run_id = res.data.check_runs.filter(check => check.name === annotationJobName)[0].id
     
         const annotation_level = numFailed + numErrored > 0 ?'failure': 'notice';
         const annotation = {
-            path: 'test',
+            path: testSrcPath,
             start_line: 0,
             end_line: 0,
             start_column: 0,
             end_column: 0,
             annotation_level,
             message: `Junit Results ran ${numTests} in ${testDuration} seconds ${numErrored} Errored, ${numFailed} Failed, ${numSkipped} Skipped`,
-          };
-        // const annotation = {
-        //   path: 'test',
-        //   start_line: 1,
-        //   end_line: 1,
-        //   start_column: 2,
-        //   end_column: 2,
-        //   annotation_level,
-        //   message: `[500] failure`,
-        // };
-
+        };
 
         const update_req = {
             ...github.context.repo,
@@ -119,6 +97,6 @@ const fs = require('fs');
         }
         await octokit.checks.update(update_req);
     } catch (error) {
-   		core.setFailed(error.message);
+        core.setFailed(error.message);
     }
 })();
